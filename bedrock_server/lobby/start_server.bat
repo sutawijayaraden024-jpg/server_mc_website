b83a15a2d66aacfa2ab41d80c3314eb187145869
Ra172 <scarlettruiss@gmail.com>
2026-06-08 20:44:23 +0700

    Fix: Update start_server.bat to set correct working directory

--- bedrock_server/lobby/start_server.bat
+++ bedrock_server/lobby/start_server.bat
@@ -2,13 +2,18 @@
 REM Bedrock Server Launcher
 REM This script starts the Minecraft Bedrock Server
 
+REM Change to the script directory
+cd /d "%~dp0"
+
 echo Starting Bedrock Server...
 echo ================================
+echo Current Directory: %cd%
 echo.
 
 REM Check if bedrock_server.exe exists
 if not exist "bedrock_server.exe" (
     echo ERROR: bedrock_server.exe not found!
+    echo Expected location: %cd%\bedrock_server.exe
     echo Please ensure bedrock_server.exe is in this directory.
     pause
     exit /b 1
@@ -16,6 +21,7 @@ if not exist "bedrock_server.exe" (
 
 REM Start the server
 echo Launching bedrock_server.exe...
+echo.
 bedrock_server.exe
 
 REM Server closed
